fe71ce94e16d78886a4408e79bff2d521a1198cd
billyluki <billylukito182@gmail.com>
2019-07-03 01:03:28 +0700

    #0000 - Add passportJS & Google Auth 2.0

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID:'116543669137-a54fd8nsi4gp8qgrk9cs64rktjon5l1f.apps.googleusercontent.com',
+    googleClientSecret:'enNfq5mvsPUJSZLYMZlWp7PW'
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const router = express.Router();
+
+// console.developers.google.com
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken',accessToken);
+          console.log('refreshToken',refreshToken);
+          console.log('profile',profile);
+      }
+    )
+);
+
+router.get(
+    '/auth/google',
+    passport.authenticate('google',{
+      scope: ['profile','email']
+    })
+);
+
+router.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Exercise Express' });
+  res.render('index', { title: 'Exercise Expresses123' });
 });
 
 module.exports = router;

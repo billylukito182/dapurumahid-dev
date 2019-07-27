f04801958e65fe824a36589125ec92db9cc7de83
billyluki <billylukito182@gmail.com>
2019-07-27 08:54:05 +0700

    #0000 - Update google distance

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
     googleClientID:'116543669137-a54fd8nsi4gp8qgrk9cs64rktjon5l1f.apps.googleusercontent.com',
     googleClientSecret:'enNfq5mvsPUJSZLYMZlWp7PW',
+    googleKey: 'AIzaSyCTiveQCY3cXPmz_0tBDFWGs7wzfjxjjsw',
     facebookClientID:'2072429546395778',
     facebookClientSecret:'cb52694c23da8243af7eef970bbd0d0e',
     mongoURI: 'mongodb://admin:admin123@ds147207.mlab.com:47207/testdapurumah',
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const distance = require('google-distance');
+const keys = require('../config/keys');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log("google distance")
+  distance.apiKey = keys.googleKey;
+  distance.get(
+      {
+        origin: 'APL Tower',
+        destination: 'Summarecon Mall Serpong'
+      },
+      function(err, data) {
+        if (err) return console.log(err);
+        console.log('distance = ',data);
+      });
   res.render('index', { title: 'Exercise Expresses123' });
 });
 
